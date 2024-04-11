08f966d4fddadbcae8ca2bc9af91bc0cab6078e1
Hayden Roney <roneyhayden@gmail.com>
2024-04-11 16:42:45 -0500

    finishes lab

--- server/app.py
+++ server/app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -14,13 +16,76 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET','POST'])
 def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+    if request.method == 'GET':
+        messages = []
+        for message in db.session.query(Message).order_by(Message.created_at.asc()):
+            message_dict = message.to_dict()
+            messages.append(message_dict)
+        response = make_response(
+            jsonify(messages),
+            200
+        )
+        return response
+    elif request.method == 'POST':
+        new_message = Message(
+            body=request.json.get("body"),
+            username=request.json.get("username"),
+        )
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+
+        response = make_response(
+            jsonify(message_dict),
+            201
+        )
+        return response
+
+@app.route('/messages/<int:id>', methods=['GET','PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+    if message == None:
+        response_body = {
+            "message": "This record does not exist in our database. Please try again."
+        }
+        response = make_response(response_body,404)
+        return response
+    else:
+        if request.method == 'GET':
+            message_dict = message.to_dict()
+            response = make_response(
+                jsonify(message_dict),
+                200
+            )
+            return response
+        elif request.method == "PATCH":
+            for attr in request.json:
+                setattr(message, attr, request.json.get(attr))
+            db.session.add(message)
+            db.session.commit()
+            message_dict = message.to_dict()
+            response = make_response(
+                jsonify(message_dict),
+                200
+            )
+            return response
+        elif request.method == "DELETE":
+            db.session.delete(message)
+            db.session.commit()
+
+            response_body = {
+                "delete_successful": True,
+                "message": "Message delted."
+            }
+            response = make_response(
+                response_body,
+                200
+            )
+            return response
 
 if __name__ == '__main__':
     app.run(port=5555)
